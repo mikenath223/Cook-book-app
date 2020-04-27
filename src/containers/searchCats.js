ca31074b0179981a0469092865cc8f5456730011
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-27 14:42:22 +0100

    [Update] Create search category container with connected dispatch to props

---
+++ src/containers/searchCats.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { FILTER } from '../actions';
+
+const mapDispatchToProps = dispatch => ({
+  filter: word => dispatch(FILTER(word))
+});
+
+const handleChange = (entry, filter) => {
+  filter(entry.target.value);
+};
+
+const Search = ({ filter }) => (
+  <div>
+    <input type="search" placeholder="Search category" onChange={e => handleChange(e, filter)} />
+  </div>
+)
+
+export default connect(null, mapDispatchToProps)(Search);
