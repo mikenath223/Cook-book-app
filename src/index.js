408089259947724cbb19444311218d32c5614dd4
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-27 14:12:11 +0100

    [Update] Create redux store, Add mealtab reducer method

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import rootReducer from './reducers'
 import App from './components/app';
 
+const store = createStore(rootReducer);
 
+console.log(store.getState());
 ReactDOM.render(
-  <App/>,
+  <Provider store={store}>
+    <App/>
+  </Provider>,
   document.getElementById('root')
 )
---
+++ src/reducers/mealTab.js
@@ -0,0 +1,11 @@
+const mealTab = (state = [], action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'SHOW MEAL':
+      return action.meal;
+    default:
+      return state;
+  }
+}
+
+export default mealTab;
