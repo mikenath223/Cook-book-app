325f39ffd441c5003ce12b9aabb9a8afaa806e3b
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-27 15:06:41 +0100

    [Update] Add routes containers switch to App component

--- src/components/app.js
+++ src/components/app.js
@@ -1,46 +1,20 @@
-import React, { useState, useEffect } from 'react'
-
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import CategoriesList from '../containers/categories';
+import CategoryTabs from '../containers/cat_tabs/category_tabs'
+import MealTab from '../containers/meal_tab/meal_tab';
 
 const App = () => {
-  const [err, setErr] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
-      .then(res => res.json())
-      .then(
-        reslt => {
-          setIsLoaded(true);
-          setCategories(reslt.categories)
-        },
-
-        err => {
-          setIsLoaded(true);
-          setErr(err);
-        }
-      )
-  }, [])
-
-  if (err) {
-    return <div>Error: {Error.message} </div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div>
-        <ul>
-          {categories.map(cat => (
-            <li key={cat.idCategory}>
-              <p>{cat.strCategory}</p>
-              <img src={cat.strCategoryThumb} alt="imgCat"/>
-              <p>{cat.strCategoryDescription}</p>
-            </li>
-            ))}
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={CategoriesList} />
+        <Route path="/category/:name" exact component={CategoryTabs} />
+        <Route path={'/:dish'} component={MealTab} />
+        <Route component={Error} />
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
 export default App;
