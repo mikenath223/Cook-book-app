b26c0a1d41ed03f4f27c4f5e185ccd7ff185ab85
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-28 00:31:09 +0100

    [Test] Add test for search field component

---
+++ src/tests/search.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { MemoryRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import Search from '../containers/searchCats';
+import rootReducer from '../reducers/index'
+
+
+afterEach(cleanup);
+const div = document.createElement('div');
+const store = createStore(rootReducer);
+
+const reduxRendering = component => ({
+  ...render(<Provider store={store}>{component}</Provider>)
+});
+
+const reduxRender = reduxRendering(
+  <MemoryRouter>
+    <Search />
+  </MemoryRouter>, div
+);
+
+it('shows the search input field with redux', () => {
+  reduxRender
+});
+
+it('checks input value', () => {
+  const { getByTestId } = reduxRendering(
+    <MemoryRouter>
+      <Search />
+    </MemoryRouter>, div
+  );
+  const input = getByTestId('entry');
+
+  expect(input.value).toBe('');
+  input.focus();
+  expect(input).toHaveFocus();
+  input.blur();
+  expect(input).not.toHaveFocus();
+});
