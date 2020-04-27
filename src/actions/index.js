b5cc63f61080325906fafcf90c7afa31cff10cfa
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-27 16:32:04 +0100

    [Feature] Create actions for display,filter and show meals

---
+++ src/actions/index.js
@@ -0,0 +1,26 @@
+const SHOWCATEGORIES = cats => ({
+  type: 'SHOW CATEGORIES',
+  cats
+});
+
+const FILTER = str => ({
+  type: 'CHANGE FILTER',
+  str
+});
+
+const DISPLAYTABS = tabs => ({
+  type: 'DISPLAY TABS',
+  tabs
+});
+
+const FILTERTABS = str => ({
+  type: 'CHANGE TAB',
+  str
+});
+
+const SHOWMEAL = meal => ({
+  type: 'SHOW MEAL',
+  meal
+});
+
+export { SHOWCATEGORIES, FILTER, DISPLAYTABS, FILTERTABS, SHOWMEAL }
