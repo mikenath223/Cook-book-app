4f28145fed478e3a3ab0d83aa494a33a17ddae4a
Michgolden Ukeje <michgoldennathan17@gmail.com>
2020-04-28 21:55:04 +0100

    [Update] Change search input field on category tab component

--- src/containers/cat_tabs/searchTabs.js
+++ src/containers/cat_tabs/searchTabs.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { FILTERTABS } from '../../actions';
+import { Link } from 'react-router-dom';
 
 const mapDispatchToProps = dispatch => ({
   filter: word => dispatch(FILTERTABS(word))
@@ -11,9 +12,17 @@ const handleChange = (entry, filter) => {
 };
 
 const SearchTabs = ({ filter }) => (
-  <div>
-    <input type="search" placeholder="Search category" onChange={e => handleChange(e, filter)} />
-  </div>
+  <nav className="navbar fixed-top navbar-dark bg-dark">
+    <Link to={'/'}><h2 className="navbar-brand home-link"> <span className="yellow">munch</span>It</h2></Link>
+    <form className="form-inline">
+      <div className="input-group">
+        <div className="input-group-prepend">
+          <span className="input-group-text" id="basic-addon1"> <img src="https://img.icons8.com/metro/16/000000/search.png" alt="" /></span>
+        </div>
+        <input type="search" className="form-control" placeholder="Search" aria-label="Search" aria-describedby="basic-addon1" data-testid="entry" onChange={e => handleChange(e, filter)} />
+      </div>
+    </form>
+  </nav>
 )
 
 export default connect(null, mapDispatchToProps)(SearchTabs);
