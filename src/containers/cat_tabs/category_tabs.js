553eda61c390e93a3a456246045ff6f7db6bcd39
mikenath223 <michgoldennathan17@gmail.com>
2020-07-30 01:27:22 +0100

    [Feature] Change timelimit on page-load animation

--- src/containers/cat_tabs/category_tabs.js
+++ src/containers/cat_tabs/category_tabs.js
@@ -37,7 +37,7 @@ const CategoryTabs = ({
           setTimeout(() => {
             displayTabs(reslt);
             setIsLoaded(true);
-          }, 1200);
+          }, 1100);
         }
       },
 
